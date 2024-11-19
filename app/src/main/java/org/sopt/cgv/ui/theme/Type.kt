1dc2963cde22b61d2ff092cd491d9591c552e985
Jaemin <kamja990510@gmail.com>
2024-11-19 10:28:42 +0900

    [ui] #3 글꼴 다운 및 FontFamily 설정

--- app/src/main/java/org/sopt/cgv/ui/theme/Type.kt
+++ app/src/main/java/org/sopt/cgv/ui/theme/Type.kt
@@ -2,10 +2,22 @@ package org.sopt.cgv.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.sopt.cgv.R
 
+val koPubDotumFontFamily = FontFamily(
+    Font(R.font.kopub_world_dotum_bold),
+    Font(R.font.kopub_world_dotum_medium),
+    Font(R.font.kopub_world_dotum_light)
+)
+
+val malgunGothicFontFamily = FontFamily(
+    Font(R.font.malgun_gothic_bold),
+    Font(R.font.malgun_gothic_regular)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
