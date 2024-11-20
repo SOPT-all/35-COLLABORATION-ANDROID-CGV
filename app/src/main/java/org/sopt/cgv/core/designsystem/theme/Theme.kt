eeac5eaed9a1598aea7ca3553fb4e8e9f2e308d0
tunaunnie <ygw021227@gmail.com>
2024-11-20 22:40:40 +0900

    UI #1 : modify import

--- app/src/main/java/org/sopt/cgv/core/designsystem/theme/Theme.kt
+++ app/src/main/java/org/sopt/cgv/core/designsystem/theme/Theme.kt
@@ -1,6 +1,5 @@
 package org.sopt.cgv.core.designsystem.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
