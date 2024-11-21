8e5edb9226df55d6aeb1f1277d02f53151e98a47
kamja0510 <kamja990510@gmail.com>
2024-11-21 22:49:05 +0900

    UI #6: TimeScreen의 배경 구현

--- app/src/main/java/org/sopt/cgv/feature/time/TimeScreen.kt
+++ app/src/main/java/org/sopt/cgv/feature/time/TimeScreen.kt
@@ -1,9 +1,15 @@
 package org.sopt.cgv.feature.time
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,6 +17,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.cgv.core.designsystem.theme.Gradient
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -19,12 +27,25 @@ fun TimeScreen(
 ) {
     var isSheetOpen by remember { mutableStateOf(true) }
 
-    Column(
+    Scaffold(
         modifier = Modifier.fillMaxSize()
-    ) {
-        Button(
-            onClick = { isSheetOpen = !isSheetOpen }
-        ) { }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(220.dp)
+                .background(Gradient)
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Button(
+                onClick = { isSheetOpen = !isSheetOpen }
+            ) { }
+        }
     }
 
     TheaterSelectionModalBottomSheet(
