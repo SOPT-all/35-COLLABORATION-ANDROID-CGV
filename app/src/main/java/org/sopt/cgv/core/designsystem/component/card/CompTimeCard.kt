50a85ee792833077a1e200d15173ef3761fc87af
tunaunnie <ygw021227@gmail.com>
2024-11-22 02:17:28 +0900

    chore #7: rm imports

--- app/src/main/java/org/sopt/cgv/core/designsystem/component/card/CompTimeCard.kt
+++ app/src/main/java/org/sopt/cgv/core/designsystem/component/card/CompTimeCard.kt
@@ -2,7 +2,6 @@ package org.sopt.cgv.core.designsystem.component.card
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,7 +21,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import org.sopt.cgv.core.designsystem.theme.*
 import org.sopt.cgv.R
 import androidx.compose.runtime.mutableStateOf
