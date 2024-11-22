45b31879ecd6653b7aa8ce56cf249d825b0e2fb5
0se0 <030ggong@gmail.com>
2024-11-22 20:02:04 +0900

    Update #14: Cgv Button Component

--- app/src/main/java/org/sopt/cgv/feature/home/component/MovieSmallCard.kt
+++ app/src/main/java/org/sopt/cgv/feature/home/component/MovieSmallCard.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.sopt.cgv.R
+import org.sopt.cgv.core.designsystem.component.button.CgvButton
 import org.sopt.cgv.core.designsystem.theme.CGVTheme
 import org.sopt.cgv.core.designsystem.theme.Gray100
 import org.sopt.cgv.core.designsystem.theme.Gray700
@@ -69,7 +70,7 @@ fun MovieSmallCard(
             )
         }
 
-        ReservationButton(
+        CgvButton(
             text = "예매",
             horizontalPadding = 4.dp,
             verticalPadding = 2.dp,
