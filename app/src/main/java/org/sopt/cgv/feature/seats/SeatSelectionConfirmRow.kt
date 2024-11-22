1c119fa0363c00d5662674d0419774743f64df50
tunaunnie <ygw021227@gmail.com>
2024-11-23 01:37:02 +0900

    [ui] #13 : SeatSelectionConfirmRow 구현

---
+++ app/src/main/java/org/sopt/cgv/feature/seats/SeatSelectionConfirmRow.kt
@@ -0,0 +1,56 @@
+package org.sopt.cgv.feature.seats
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import org.sopt.cgv.core.designsystem.theme.Typography
+
+@Composable
+fun SeatSelectionConfirmRow(
+    label: String,
+    price: String,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = label,
+            style = Typography.body4_m_15,
+            color = Color.Black
+        )
+
+        Row(
+            modifier = Modifier,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Text(
+                text = price,
+                style = Typography.head8_b_20,
+                color = Color.Black
+            )
+            Text(
+                text = "원",
+                style = Typography.head4_b_15,
+                color = Color.Black
+            )
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SeatSelectionConfirmRowPreview(){
+    SeatSelectionConfirmRow(
+        label = "일반1",
+        price = "14,000"
+    )
+}
