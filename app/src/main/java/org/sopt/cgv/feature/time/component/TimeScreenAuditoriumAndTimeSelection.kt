2f2e9eae618cb02e8dad912cba9b7a792298272b
kamja0510 <kamja990510@gmail.com>
2024-11-25 23:44:27 +0900

    Feature #10: 지원이가 만들어준 TimeCard 훔쳐오기!

---
+++ app/src/main/java/org/sopt/cgv/feature/time/component/TimeScreenAuditoriumAndTimeSelection.kt
@@ -0,0 +1,15 @@
+package org.sopt.cgv.feature.time.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TimeScreenAuditorioumAndTimeSelection(){
+
+}
+
+@Preview
+@Composable
+fun TimeScreenAuditorioumAndTimeSelectionPreview(){
+    TimeScreenAuditorioumAndTimeSelection()
+}
