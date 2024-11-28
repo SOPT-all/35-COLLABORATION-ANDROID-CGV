bcfd328b3eebfd3ee0f1144325562d7b0435fc1a
kamja0510 <kamja990510@gmail.com>
2024-11-28 19:03:57 +0900

    Delete #18 : MainScreen 쓸모없는 import 삭제

--- app/src/main/java/org/sopt/cgv/feature/main/MainActivity.kt
+++ app/src/main/java/org/sopt/cgv/feature/main/MainActivity.kt
@@ -5,18 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import org.sopt.cgv.core.designsystem.theme.CGVTheme
 import org.sopt.cgv.feature.home.HomeRoute
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.NavHost
 import org.sopt.cgv.feature.time.TimeRoute
 import org.sopt.cgv.feature.time.TimeScreenViewModel
 
@@ -52,9 +50,9 @@ fun MainScreen() {
                 onReservationClick = { navController.navigate("time") }
             )
         }
-        composable("time"){
+        composable("time") {
             TimeRoute(
-                navigateToSeat = { navController.navigate("seats")},
+                navigateToSeat = { navController.navigate("seats") },
                 timeScreenViewModel = timeScreenViewModel
             )
         }
